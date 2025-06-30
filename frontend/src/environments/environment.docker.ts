6e5259bf81f63ed31535deef6603c9798b3123f6
Marco Rosa <marco.rosa@sap.com>
2025-06-30 16:23:19 +0200

    Fix docker connections

--- frontend/src/environments/environment.docker.ts
+++ frontend/src/environments/environment.docker.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  backend_url: 'http://backend:8080/process',
-  backend_url_ws: 'ws://backend:8080/agent',
-  api_url: 'http://backend:8080',
+  backend_url: 'http://localhost:8080/process',
+  backend_url_ws: 'ws://localhost:8080/agent',
+  api_url: 'http://localhost:8080',
 };
