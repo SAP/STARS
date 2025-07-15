29647ebd05c78272ae0c97a754cd0b572e275b48
Caroline BANCHEREAU <Caroline BANCHEREAU>
2025-07-15 11:10:53 +0200

    aligned frontend

--- frontend/src/app/utils/utils.ts
+++ frontend/src/app/utils/utils.ts
@@ -1,11 +1,11 @@
 // export function generateModelName(vendor: string, modelType: string, version: string, specialization: string, other: string, withVendor = true): string {
-export function generateModelName(data: any[], vendor: string): any {
+export function generateModelName(data: any[], vendor: string): Record<string, string[]> {
   const result: Record<string, string[]> = {};
 
   data.forEach(row => {
     const vendorName = vendor === '' ? row['vendor'] : vendor; // Si vendor est vide, on prend row['vendor'], sinon on utilise vendor existant
 
-    const model = [row['modelType'], row['version'], row['specialization'], row['other']]
+    const model = [row['modleName']]
       .filter(value => value)
       .join('-')
       .replace(/\s+/g, ' ')
@@ -20,19 +20,20 @@ export function generateModelName(data: any[], vendor: string): any {
   return result;
 }
 
-export function splitModelName(vendor: string, model: string): string[] {
-  if (model.length < 18) return [vendor, model]; // No need to split
+// Function to split model names longer than 18 characters into two parts to fit in the ui y-axis
+export function splitModelName(model: string): string[] {
+  if (model.length < 18) return [capitalizeFirstLetter(model)]; // No need to split
 
   // Find the last "-" before the 20th character
   const cutoffIndex = model.lastIndexOf('-', 20);
 
   if (cutoffIndex === -1) {
     // If no "-" found before 20, force split at 20
-    return [vendor, model.slice(0, 20), model.slice(20)];
+    return [model.slice(0, 20), model.slice(20)];
   }
 
   // Split at the last "-" before 20
-  return [vendor, model.slice(0, cutoffIndex), model.slice(cutoffIndex + 1)].map(capitalizeFirstLetter);
+  return [capitalizeFirstLetter(model.slice(0, cutoffIndex)), model.slice(cutoffIndex + 1)];
 }
 
 export function capitalizeFirstLetter(str: string): string {
